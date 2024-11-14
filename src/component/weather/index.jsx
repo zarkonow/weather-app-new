4726659776b0c8a05643228156eefc7f5b562ea2
zarkonow <stojanov1987@gmail.com>
2024-11-14 23:40:36 +0100

    *added useState

--- src/component/weather/index.jsx
+++ src/component/weather/index.jsx
@@ -1,15 +1,24 @@
-
-
-
-
-import React from 'react'
-import Search from '../search'
+import Search from "../search";
+import { useState } from "react";
 
 export default function Weather() {
-  return (
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weather, setWeather] = useState(null);
+
+
+  async function handleSearch() {
     
+  }
+
+  return (
     <div>
-        <Search/>
-        Weather</div>
-  )
+      <Search 
+      search={search} 
+      setSearch={setSearch} 
+      handleSearch={handleSearch}
+      />
+      Weather
+    </div>
+  );
 }
